dd18d91075ed45d7349cdf4f8a5cd8c98bec9763
MaysenTG <greenwood.maysen@gmail.com>
2021-10-25 17:56:30 +1300

    change homepage name link

--- src/Components/layouts/Navbar.js
+++ src/Components/layouts/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
             <div className="container">
                 <nav className="navbar navbar-light">
                     <div className="container-fluid">
-                        <a href="/todo" className="navbar-brand">To Do List App</a>
+                        <a href="/react-todo-app" className="navbar-brand">To Do List App</a>
                         <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search for an item" aria-label="Search"/>
                             <button className="btn btn-outline-success" type="submit">Search</button>
