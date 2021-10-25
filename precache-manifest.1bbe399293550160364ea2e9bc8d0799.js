ae604797f0d8b86336e882823b35851e9f87426c
MaysenTG <greenwood.maysen@gmail.com>
2021-10-25 17:54:58 +1300

    Updates

---
+++ precache-manifest.1bbe399293550160364ea2e9bc8d0799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547c419851b5caaeb150bae14ff8d181",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "7591565a528433be39ef",
+    "url": "/react-todo-app/static/css/main.79908598.chunk.css"
+  },
+  {
+    "revision": "241e8a40c89a334b7744",
+    "url": "/react-todo-app/static/js/2.88ef81bd.chunk.js"
+  },
+  {
+    "revision": "d58d341c47f1a54831bbdcb382df5f39",
+    "url": "/react-todo-app/static/js/2.88ef81bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7591565a528433be39ef",
+    "url": "/react-todo-app/static/js/main.86ae3c12.chunk.js"
+  },
+  {
+    "revision": "c3cdebc05aaa0be0cb46",
+    "url": "/react-todo-app/static/js/runtime-main.c8e4e8b1.js"
+  }
+]);
